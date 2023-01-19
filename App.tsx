d1e47ca698d35f35ef21250cba41594601050f2a
Aldrin Tingson <aldrintingson@gmail.com>
2023-01-19 19:53:53 +0800

    initial implementation of fetching news

--- App.tsx
+++ App.tsx
@@ -1,87 +1,82 @@
 import React from "react";
 import {
-  Text,
-  Link,
-  HStack,
-  Center,
-  Heading,
-  Switch,
-  useColorMode,
-  NativeBaseProvider,
-  extendTheme,
-  VStack,
-  Box,
+    Divider,
+    extendTheme,
+    HStack,
+    Icon,
+    Input,
+    NativeBaseProvider,
+    Switch,
+    Text,
+    useColorMode,
+    VStack,
 } from "native-base";
-import NativeBaseIcon from "./components/NativeBaseIcon";
+import {MaterialIcons} from "@expo/vector-icons";
+import {SafeAreaView} from "react-native";
+import useAppStore from "./store/app-store";
 
 // Define the config
 const config = {
-  useSystemColorMode: false,
-  initialColorMode: "dark",
+    useSystemColorMode: false,
+    initialColorMode: "dark",
 };
 
 // extend the theme
-export const theme = extendTheme({ config });
+export const theme = extendTheme({config});
 type MyThemeType = typeof theme;
 declare module "native-base" {
-  interface ICustomTheme extends MyThemeType {}
+    interface ICustomTheme extends MyThemeType {
+    }
 }
 export default function App() {
-  return (
-    <NativeBaseProvider>
-      <Center
-        _dark={{ bg: "blueGray.900" }}
-        _light={{ bg: "blueGray.50" }}
-        px={4}
-        flex={1}
-      >
-        <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <HStack space={2} alignItems="center">
-            <Text>Edit</Text>
-            <Box
-              _web={{
-                _text: {
-                  fontFamily: "monospace",
-                  fontSize: "sm",
-                },
-              }}
-              px={2}
-              py={1}
-              _dark={{ bg: "blueGray.800" }}
-              _light={{ bg: "blueGray.200" }}
-            >
-              App.js
-            </Box>
-            <Text>and save to reload.</Text>
-          </HStack>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={"xl"}>
-              Learn NativeBase
-            </Text>
-          </Link>
-          <ToggleDarkMode />
-        </VStack>
-      </Center>
-    </NativeBaseProvider>
-  );
+    const appStore = useAppStore()
+    const {articles, fetchArticles} = appStore
+    return (
+        <NativeBaseProvider>
+            <SafeAreaView>
+                <VStack w="100%" space={5} alignSelf="center">
+                    <Input placeholder="Search News" width="100%" borderRadius="4" py="3" px="1"
+                       fontSize="14"
+                       InputLeftElement={
+                           <Icon m="2" ml="3" size="6" color="gray.400" as={<MaterialIcons name="search"/>}/>
+                       }
+                       onSubmitEditing={async (e) => {
+                           console.log(e.nativeEvent.text)
+                           await fetchArticles(e.nativeEvent.text)
+                       }}
+                    />
+                    {articles.map((a, index) => (
+                        <VStack key={`article=${index}`}>
+                            <Text fontWeight={"bold"}>
+                                {a.title}
+                            </Text>
+                            <Text>
+                                {a.description}
+                            </Text>
+                            <Divider  />
+                        </VStack>
+                        ))
+                    }
+                </VStack>
+            </SafeAreaView>
+        </NativeBaseProvider>
+    );
 }
 
 // Color Switch Component
 function ToggleDarkMode() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  return (
-    <HStack space={2} alignItems="center">
-      <Text>Dark</Text>
-      <Switch
-        isChecked={colorMode === "light"}
-        onToggle={toggleColorMode}
-        aria-label={
-          colorMode === "light" ? "switch to dark mode" : "switch to light mode"
-        }
-      />
-      <Text>Light</Text>
-    </HStack>
-  );
+    const {colorMode, toggleColorMode} = useColorMode();
+    return (
+        <HStack space={2} alignItems="center">
+            <Text>Dark</Text>
+            <Switch
+                isChecked={colorMode === "light"}
+                onToggle={toggleColorMode}
+                aria-label={
+                    colorMode === "light" ? "switch to dark mode" : "switch to light mode"
+                }
+            />
+            <Text>Light</Text>
+        </HStack>
+    );
 }
---
+++ store/app-store.ts
@@ -0,0 +1,63 @@
+import {create} from 'zustand'
+
+export enum FetchStatus {
+    IDLE,
+    FETCHING,
+    SUCCESS,
+    ERROR
+}
+
+export interface Article {
+    title: string
+    author: string
+    description: string
+}
+
+export interface FetchResponse {
+    articles: Article[]
+}
+
+export interface AppStore {
+    pageNumber: number
+    status: FetchStatus
+    fetchArticles: (query: string) => Promise<void>
+
+    articles: Article[]
+
+    query: string
+}
+
+export const useAppStore = create<AppStore>((set, get) => ({
+    status: FetchStatus.IDLE,
+    pageNumber: 1,
+    articles: [],
+    query: "",
+    fetchArticles: async (query: string) => {
+        set(state => ({...state, status: FetchStatus.FETCHING}))
+        const resp = await fetch('https://newsapi.org/v2/everything?' + new URLSearchParams({
+            apiKey: "183daca270264bad86fc5b72972fb82a",
+            q: query,
+            // searchIn: "title",
+            pageNumber: get().pageNumber.toString(),
+            pageSize: "10"
+        }))
+        // console.log("resp:", JSON.stringify(resp))
+
+        if (resp.ok) {
+            set(state => ({...state, status: FetchStatus.SUCCESS}))
+            const data: FetchResponse = await resp.json()
+            console.log("data", JSON.stringify(data))
+            const isSameQuery = query === get().query
+            const articles = isSameQuery ? [...get().articles, ...data.articles] : data.articles
+            console.log("articles: ", articles)
+            const pageNumber = isSameQuery ? get().pageNumber + 1 : 1
+
+            set(state => ({...state, articles: articles, pageNumber, query, status: FetchStatus.IDLE}))
+        } else {
+            console.log("2")
+            set(state => ({...state, query, status: FetchStatus.ERROR}))
+        }
+    }
+}))
+
+export default useAppStore
